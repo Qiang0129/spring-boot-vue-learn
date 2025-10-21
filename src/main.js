f481881e3c653ff3f0399210c1c86367687ad165
Qiang0129 <mrqiang0129@gmail.com>
2025-10-21 15:28:20 +0800

    下载引入sass依赖,将之前的home.vue文件放到test.vue文件中,新的home.vue文件用来学习element-plus组件的使用

--- src/main.js
+++ src/main.js
@@ -6,11 +6,18 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import './assets/globa.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// import '@/assets/index.scss'
+
 const app = createApp(App)
+
 
 app.use(router)
 // 全局使用element-plus，并设置为中文
 app.use(ElementPlus, {
   locale: zhCn
 })
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,31 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// 导入全局样式
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ElementPlus from 'unplugin-element-plus/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // 按需定制主题配置
+    ElementPlus({
+      useSource: true,
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver({importStyle: 'sass'})],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver({importStyle: 'sass'})],
+    }),
   ],
+
+
   //修改端口号为8080，否则默认为5371
   server: {
     host: true,
@@ -19,5 +37,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `@use "@/assets/index.scss" as *;`,
+      }
+    }
   },
 })
